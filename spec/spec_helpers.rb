9ddff62a14acd14ef6d8abe470f7e9dc782d62d3
John Mettraux <jmettraux@gmail.com>
2017-11-26 12:59:21 +0900

    Display input/tree length in tree pretty print

--- spec/spec_helpers.rb
+++ spec/spec_helpers.rb
@@ -49,6 +49,7 @@ module Helpers
     sc1 = "[0;33m" # string color
     c1 = "[0;32m" # result 1 color
     rc = "[0;0m" # reset color
+    rdc = "[1;31m" # red color
 
     o, l = n['offset'], n['length']
     s = n['input']['string'][o..-1]
@@ -77,6 +78,13 @@ module Helpers
     puts sio.string
 
     n['children'].each { |c| print_tree(c, indent + '  ') }
+
+    if indent == ''
+      il = n['input']['string'].length
+      tl = n['length']; tl = "#{rdc}#{tl}#{rc}" if tl != il
+      puts "├─ input length:  #{il}"
+      puts "└─ tree length:   #{tl}"
+    end
   end
 end
 RSpec.configure { |c| c.include(Helpers) }
