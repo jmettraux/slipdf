852949bd3c7193639d1446be5efc389feb5162bf
John Mettraux <jmettraux@gmail.com>
2017-11-28 15:11:48 +0900

    Sort template spec by index

--- spec/template_spec.rb
+++ spec/template_spec.rb
@@ -14,34 +14,35 @@ describe 'Slipdf' do
 
     describe '()' do
 
-      Dir['spec/t_*.slim'].each do |slim|
+      Dir['spec/t_*.slim']
+        .sort_by { |slim| slim.match(/\d+/)[0].to_i }
+        .each { |slim|
 
-        i = slim.match(/\d+/)[0].to_i
+          i = slim.match(/\d+/)[0].to_i
 
-        sli =
-          File.basename(slim)
-        src =
-          File.read(slim).inspect
-        ctx =
-          JSON.dump(
-            File.exist?("spec/t_#{i}_ctx.yaml") ?
-            YAML.load(File.read("spec/t_#{i}_ctx.yaml")) :
-            eval(File.read("spec/t_#{i}_ctx.rb")))
-        res =
-          File.exist?("spec/t_#{i}.yaml") ?
-          YAML.load(File.read("spec/t_#{i}.yaml")) :
-          eval(File.read("spec/t_#{i}.rb"))
+          sli =
+            File.basename(slim)
+          src =
+            File.read(slim).inspect
+          ctx =
+            JSON.dump(
+              File.exist?("spec/t_#{i}_ctx.yaml") ?
+              YAML.load(File.read("spec/t_#{i}_ctx.yaml")) :
+              eval(File.read("spec/t_#{i}_ctx.rb")))
+          res =
+            File.exist?("spec/t_#{i}.yaml") ?
+            YAML.load(File.read("spec/t_#{i}.yaml")) :
+            eval(File.read("spec/t_#{i}.rb"))
 
-        it "generates a pdfmake document for #{sli}" do
+          it "generates a pdfmake document for #{sli}" do
 
-          #print_tree(js "var src = #{src}; return Slipdf.debug(src, 3);")
-          puts '-' * 80; pp(js "return Slipdf.prepare(#{src});")
-          puts '-' * 80; pp(js "return Slipdf.compile(#{src})(#{ctx});")
-          puts '-' * 80; puts YAML.dump(js "return Slipdf.compile(#{src})(#{ctx});")
+            #print_tree(js "var src = #{src}; return Slipdf.debug(src, 3);")
+            puts '-' * 80; pp(js "return Slipdf.prepare(#{src});")
+            puts '-' * 80; pp(js "return Slipdf.compile(#{src})(#{ctx});")
+            puts '-' * 80; puts YAML.dump(js "return Slipdf.compile(#{src})(#{ctx});")
 
-          expect(js("return Slipdf.compile(#{src})(#{ctx});")).to eq(res)
-        end
-      end
+            expect(js("return Slipdf.compile(#{src})(#{ctx});")).to eq(res)
+          end }
     end
   end
 end
