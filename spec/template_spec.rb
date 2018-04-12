32a373d8d202c7dd39c9153f67bbd5e0afcf664b
John Mettraux <jmettraux@gmail.com>
2018-04-12 20:30:49 +0900

    Ensure empty table cells are OK

--- spec/template_spec.rb
+++ spec/template_spec.rb
@@ -38,9 +38,14 @@ describe 'Slipdf' do
 
             #print_tree(js "var src = #{src}; return Slipdf.debug(src, 3);")
 
-            #puts '-' * 80; pp(js "return Slipdf.prepare(#{src});")
-            #puts '-' * 80; pp(js "return Slipdf.compile(#{src})(#{ctx});")
-            #puts '-' * 80; puts YAML.dump(js "return Slipdf.compile(#{src})(#{ctx});")
+            #puts '-' * 80
+            #pp(js "return Slipdf.prepare(#{src});")
+              #
+            #puts '-' * 80
+            #pp(js "return Slipdf.compile(#{src})(#{ctx});")
+              #
+            #puts '-' * 80
+            #puts YAML.dump(js "return Slipdf.compile(#{src})(#{ctx});")
 
             expect(js("return Slipdf.compile(#{src})(#{ctx});")).to eq(res)
           end }
